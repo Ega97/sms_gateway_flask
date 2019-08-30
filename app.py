a24fbe32c8bba448c47fd9be91ae39463a30c686
Ega97 <aaliyando12@gmail.com>
2019-08-30 17:24:07 +0700

    data siswa

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_material import Material
+
+
+app = Flask(__name__)
+Material(app)
+
+
+# Index
+@app.route('/i_data_siswa')
+def i_data_siswa():
+    return render_template('i_data_siswa.html')
+
+
+@app.route('/')
+def index():
+
+    return render_template("index_bk.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
